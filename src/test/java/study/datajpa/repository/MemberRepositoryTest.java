4b4ceb45bc16ce2d52cde0eb942ba22b82ecb3ba
76jin <ranian129@gmail.com>
2023-05-28 21:29:36 +0900

    @Query, 값, DTO 조회하기

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 
@@ -17,6 +19,7 @@ import static org.assertj.core.api.Assertions.*;
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @Test
     void testMember() {
@@ -108,5 +111,35 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getAge()).isEqualTo(member1.getAge());
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get(0)).isEqualTo(member1);
+    }
+
+    @Test
+    void testFindByUsernameList() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<String> result = memberRepository.findUsernameList();
+
+        for (String username : result) {
+            System.out.println("## username:" + username);
+        }
+    }
+
+    @Test
+    void testMemberDto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member member1 = new Member("AAA", 10);
+        member1.setTeam(team);
+        memberRepository.save(member1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+        for (MemberDto dto : memberDto) {
+            System.out.println("## dto:" + dto);
+        }
     }
 }
